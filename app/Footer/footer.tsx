cb12c4eabdab56e1076d8dd33d727343b815c130
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2025-03-29 15:08:34 -0500

    Added contact link to footer

--- app/Footer/footer.tsx
+++ app/Footer/footer.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav className="bg-gray-900 text-white py-4 shadow-lg fixed bottom-0 left-0 w-full z-50">
             <div className="container mx-auto px-6 flex justify-between items-center">
                 <div className="hidden md:flex space-x-8 text-lg font-semibold tracking-wide">
-                    <p>Created by the Goatiendas</p>
+                    <Link href="https://latienditamckinney.org/contact" className="hover:text-gray-300 transition duration-300">Contact La Tiendita</Link>
                 </div>
             </div>
         </nav>
