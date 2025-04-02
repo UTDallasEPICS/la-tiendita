6c0aded4a703170ac8df5b2fca458d1560e545fa
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2025-04-02 15:50:23 -0500

    Added a "type" field to the question module to determine the type of question.

--- app/Survey/Module.tsx
+++ app/Survey/Module.tsx
@@ -2,6 +2,7 @@ import Scale_Choice_Answer_Form from "./Scale_Choice_Answer_Form";
 
 interface ModuleProps {
     question: string;
+    type: string;
 }
 
 export default function Module(props: ModuleProps) {
@@ -9,7 +10,6 @@ export default function Module(props: ModuleProps) {
       <div className="flex items-center w-full space-x-6 p-6 block border-2 border-blue-500 rounded-lg mb-6 max-h-[250px] overflow-hidden">
         <div className="flex flex-col space-y-2">
             <h1 className="text-xl font-semibold text-customGray">{props.question}</h1>
-            <h2 className="text-lg text-customGray">{props.question}</h2>
             <Scale_Choice_Answer_Form numChoices={5} lowLabel="Strongly Disagree" highLabel="Strongly Agree"></Scale_Choice_Answer_Form>
         </div>
       </div>
--- app/Survey/page.tsx
+++ app/Survey/page.tsx
@@ -4,13 +4,9 @@ export default function Survey() {
   return (
     <section className="mt-20 p-5 max-w-5xl mx-auto">
       <div className="flex flex-wrap justify-center gap-6">
-        <Module question="Do androids dream of electric sheep?"></Module>
-        <Module question="Do androids dream of electric sheep?"></Module>
-        <Module question="Do androids dream of electric sheep?"></Module>
-        <Module question="Do androids dream of electric sheep?"></Module>
-        <Module question="Do androids dream of electric sheep?"></Module>
-        <Module question="Do androids dream of electric sheep?"></Module>
-        <Module question="Do androids dream of electric sheep?"></Module>
+        <Module question="Do androids dream of electric sheep?" type="scale"></Module>
+        <Module question="Do androids dream of electric sheep?" type="multi"></Module>
+        <Module question="Do androids dream of electric sheep?" type="free"></Module>
       </div>
     </section>
   );
