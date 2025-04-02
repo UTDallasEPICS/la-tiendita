45bd3340d994a878f0838dd83aefa7592d12c551
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2025-04-02 15:36:24 -0500

    Created and added dummy pages. Linked assessments and dummy pages to navbar.

--- app/Navbar/navbar.tsx
+++ app/Navbar/navbar.tsx
@@ -8,10 +8,10 @@ export default function Navbar() {
             <div className="container mx-auto px-6 flex justify-between items-center">
                 <Image src={logo} className="h-14 w-auto" alt="logo" />
                 <div className="hidden md:flex space-x-8 text-lg font-semibold tracking-wide">
-                    <Link href="#" className="hover:text-gray-300 transition duration-300">About</Link>
-                    <Link href="#" className="hover:text-gray-300 transition duration-300">Assessments</Link>
-                    <Link href="#" className="hover:text-gray-300 transition duration-300">Results</Link>
-                    <Link href="#" className="hover:text-gray-300 transition duration-300">Profile</Link>
+                    <Link href="./about" className="hover:text-gray-300 transition duration-300">About</Link>
+                    <Link href="./assessments" className="hover:text-gray-300 transition duration-300">Assessments</Link>
+                    <Link href="./results" className="hover:text-gray-300 transition duration-300">Results</Link>
+                    <Link href="./profile" className="hover:text-gray-300 transition duration-300">Profile</Link>
                 </div>
             </div>
         </nav>
