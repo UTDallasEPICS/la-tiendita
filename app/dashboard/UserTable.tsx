b9eb964564068532e118a962bc20d7b1680d4a94
mikehquan19 <hoangphucquan19@gmail.com>
2025-04-13 03:12:22 -0500

    Refactor the code a bit to look a little better

--- app/dashboard/UserTable.tsx
+++ app/dashboard/UserTable.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState, useEffect, FormEvent, ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
 
 interface User {
   id: number;
@@ -14,11 +15,13 @@ const BASE_URL = 'http://localhost:3000/api/users?'
 export default function UserTable() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [userData, setUserData] = useState<User[]>([]);
-  const [filterData, setFilterData] = useState<{[key: string]: any}>({
-    name: "", 
-    email: "", 
+  const [filterData, setFilterData] = useState<{ [key: string]: any }>({
+    name: "",
+    email: "",
     role: ""
   })
+
+  const router = useRouter()
 
   useEffect(() => {
     // Fetch the user data from the API
@@ -26,6 +29,7 @@ export default function UserTable() {
       try {
         const response: Response = await fetch(BASE_URL)
         const data: any = await response.json()
+        
         setUserData(data)
         setIsLoading(false)
       }
@@ -54,98 +58,99 @@ export default function UserTable() {
       const data: any = await response.json()
       setUserData(data)
     }
-    catch(error: any) {
+    catch (error: any) {
       console.log(error);
     }
   }
 
   // Render the table right after data has been fetched successfully 
-  if (!isLoading) {
-    return (
-      <div className="w-3/4 mx-auto mt-10">
-        <div className="flex justify-between">
-          <h1 className="text-2xl font-bold">User Profiles ({userData.length}):</h1>
-          <form className="flex gap-2 text-sm" onSubmit={handleSubmit}>
-            <div className="text-text">
-              <label className="font-bold">Name: </label>
-              <input
-                className="border border-gray-500 rounded-lg px-2 py-1"
-                type="text"
-                name="name"
-                onChange={(e: ChangeEvent<HTMLInputElement>) => 
-                  setFilterData({...filterData, name: e.target.value})}
-              />
-            </div>
-            <div className="text-text">
-              <label className="font-bold">Email: </label>
-              <input
-                className="border border-gray-500 rounded-lg px-2 py-1"
-                type="text"
-                name="email"
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setFilterData({...filterData, email: e.target.value})
-                }
-              />
-            </div>
-            <div className="text-text">
-              <label className="font-bold">Role: </label>
-              <select 
-                name="role" 
-                className="border border-gray-500 rounded-lg py-1"
-                onChange={(e: ChangeEvent<HTMLSelectElement>) => 
-                  setFilterData({...filterData, role: e.target.value})
-                }
-              >
+  if (isLoading) {
+    return <div>LOADING...</div>
+  }
+
+  return (
+    <div className="w-3/4 mx-auto mt-10">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">User Profiles ({userData.length}):</h1>
+        <form className="flex gap-2 text-sm" onSubmit={handleSubmit}>
+          <div className="text-text">
+            <label className="font-bold">Name: </label>
+            <input
+              className="border border-gray-500 rounded-lg px-2 py-1"
+              type="text"
+              name="name"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setFilterData({ ...filterData, name: e.target.value })}
+            />
+          </div>
+          <div className="text-text">
+            <label className="font-bold">Email: </label>
+            <input
+              className="border border-gray-500 rounded-lg px-2 py-1"
+              type="text"
+              name="email"
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setFilterData({ ...filterData, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="text-text">
+            <label className="font-bold">Role: </label>
+            <select
+              name="role"
+              className="border border-gray-500 rounded-lg py-1"
+              onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                setFilterData({ ...filterData, role: e.target.value })
+              }
+            >
               {["", "ADMIN", "USER"].map(str => <option key={str} value={str}>{str}</option>)}
             </select>
-            </div>
-            <button className="p-1 rounded-xl bg-accent text-white font-semibold" type="submit">Search</button>
-          </form>
-        </div>
-        <table className="w-full border-collapse border border-gray-500 mt-2 rounded">
-          <thead>
-            <tr className="bg-secondary text-white text-center text-gray-700">
-              {["ID", "Name", "Email", "Date Joined", "Role", "Answer", "Edit"].map(field =>
-                <th key={field} className="py-2 text-lg">{field}</th>
-              )}
-            </tr>
-          </thead>
-          <tbody>
-            {userData.map(user =>
-              <tr 
-                key={user.id} 
-                className="even:bg-background odd:bg-white border border-gray-500 text-text font-semibold text-center"
-              >
-                <td className="py-4" style={{ width: "10%" }}>{user.id}</td>
-                <td style={{ width: "20%" }}>{user.name}</td>
-                <td style={{ width: "20%" }}>{user.email}</td>
-                <td style={{ width: "15%" }}>{user.dateCreated.slice(0, 10)}</td>
-                <td style={{ width: "10%" }}>{user.role}</td>
-                <td style={{ width: "12.5%" }}>
-                  <button className="bg-accent w-3/5 text-white shadow-lg hover:shadow-xl rounded-xl p-1 my-1">
-                    Answer
-                  </button>
-                </td>
-                <td style={{ width: "12.5%" }}>
-                  <button className="bg-accent w-3/5 text-white shadow-lg hover:shadow-xl rounded-xl p-1 my-1">
-                    Edit
-                  </button>
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-        <div className="flex justify-center mt-4 text-lg text-text">
-          <button className="rounded-l-md px-2 bg-accent text-white">Prev</button>
-          {[1, 2, 3, 4, 5].map(num =>
-            <button 
-              key={num} 
-              className="border border-gray-500 px-2" 
-            >{num}</button>
-          )}
-          <button className="rounded-r-md px-2 bg-accent text-white">Next</button>
-        </div>
+          </div>
+          <button className="p-1 rounded-xl bg-accent text-white font-semibold" type="submit">Search</button>
+        </form>
       </div>
-    );
-  }
+      <table className="w-full border-collapse border border-gray-500 mt-2 rounded">
+        <thead>
+          <tr className="bg-secondary text-white text-center text-gray-700">
+            {["ID", "Name", "Email", "Date Joined", "Role", "Answer", "Edit"].map(field =>
+              <th key={field} className="py-2 text-lg">{field}</th>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map(user =>
+            <tr
+              key={user.id}
+              className="even:bg-background odd:bg-white border border-gray-500 text-text font-semibold text-center"
+            >
+              <td className="py-4" style={{ width: "10%" }}>{user.id}</td>
+              <td style={{ width: "20%" }}>{user.name}</td>
+              <td style={{ width: "20%" }}>{user.email}</td>
+              <td style={{ width: "15%" }}>{user.dateCreated.slice(0, 10)}</td>
+              <td style={{ width: "10%" }}>{user.role}</td>
+              <td style={{ width: "12.5%" }}>
+                <button
+                  onClick={() => router.push(`./user_results/${user.id}`)}
+                  className="bg-accent w-3/5 text-white shadow-lg hover:shadow-xl rounded-xl p-1 my-1"
+                  disabled={user.role === "ADMIN"}
+                >Answer</button>
+              </td>
+              <td style={{ width: "12.5%" }}>
+                <button className="bg-accent w-3/5 text-white shadow-lg hover:shadow-xl rounded-xl p-1 my-1">
+                  Edit
+                </button>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <div className="flex justify-center mt-4 text-lg text-text">
+        <button className="rounded-l-md px-2 bg-accent text-white">Prev</button>
+        {[1, 2, 3, 4, 5].map(num =>
+          <button key={num} className="border border-gray-500 px-2">{num}</button>
+        )}
+        <button className="rounded-r-md px-2 bg-accent text-white">Next</button>
+      </div>
+    </div>
+  );
 }
